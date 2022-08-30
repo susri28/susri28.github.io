c2c4a0201668011347c6276f7bb7d0e59491f4ed
susri28 <108443640+susri28@users.noreply.github.com>
2022-08-30 22:48:29 +0700

    Add files via upload

---
+++ stock/stock.php
@@ -0,0 +1,332 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<?php 
+    include '../dbconnect.php';
+    include 'cek.php';
+
+    if(isset($_POST['update'])){
+        $idx = $_POST['idbrg'];
+        $nama = $_POST['nama'];
+        $jenis = $_POST['jenis'];
+        $merk = $_POST['merk'];
+        $ukuran = $_POST['ukuran'];
+        $satuan = $_POST['satuan'];
+        $lokasi = $_POST['lokasi'];
+
+        $updatedata = mysqli_query($conn,"update sstock_brg set nama='$nama', jenis='$jenis', merk='$merk', ukuran='$ukuran', satuan='$satuan', lokasi='$lokasi' where idx='$idx'");
+        
+        //cek apakah berhasil
+        if ($updatedata){
+
+            echo " <div class='alert alert-success'>
+                <strong>Success!</strong> Redirecting you back in 1 seconds.
+              </div>
+            <meta http-equiv='refresh' content='1; url= stock.php'/>  ";
+            } else { echo "<div class='alert alert-warning'>
+                <strong>Failed!</strong> Redirecting you back in 1 seconds.
+              </div>
+             <meta http-equiv='refresh' content='1; url= stock.php'/> ";
+            }
+    };
+
+    if(isset($_POST['hapus'])){
+        $idx = $_POST['idbrg'];
+
+        $delete = mysqli_query($conn,"delete from sstock_brg where idx='$idx'");
+        //hapus juga semua data barang ini di tabel keluar-masuk
+        $deltabelkeluar = mysqli_query($conn,"delete from sbrg_keluar where id='$idx'");
+        $deltabelmasuk = mysqli_query($conn,"delete from sbrg_masuk where id='$idx'");
+        
+        //cek apakah berhasil
+        if ($delete && $deltabelkeluar && $deltabelmasuk){
+
+            echo " <div class='alert alert-success'>
+                <strong>Success!</strong> Redirecting you back in 1 seconds.
+              </div>
+            <meta http-equiv='refresh' content='1; url= stock.php'/>  ";
+            } else { echo "<div class='alert alert-warning'>
+                <strong>Failed!</strong> Redirecting you back in 1 seconds.
+              </div>
+             <meta http-equiv='refresh' content='1; url= stock.php'/> ";
+            }
+    };
+	?>
+
+<head>
+
+    <meta charset="utf-8">
+	<link rel="icon" 
+      type="image/png" 
+      href="../favicon.png">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Desa Cantik 2022</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/png" href="assets/images/icon/favicon.ico">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/metisMenu.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/slicknav.min.css">
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144808195-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-144808195-1');
+	</script>
+    <!-- amchart css -->
+    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+	<!-- Start datatable css -->
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.jqueryui.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.5.2/css/buttons.dataTables.min.css">
+	
+    <!-- others css -->
+    <link rel="stylesheet" href="assets/css/typography.css">
+    <link rel="stylesheet" href="assets/css/default-css.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <!-- modernizr css -->
+    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+
+</head>
+
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- preloader area start -->
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- page container area start -->
+    <div class="page-container">
+        <!-- sidebar menu area start -->
+        <div class="sidebar-menu">
+            <div class="sidebar-header">
+                    <a href="index.php"><img src="../cantik.png" alt="cantik" width="100%"></a>
+            </div>
+            <div class="main-menu">
+                <div class="menu-inner">
+                    <nav>
+                        <ul class="metismenu" id="menu">
+							<li><a href="index.php"><span>Notes</span></a></li>
+                            <li class="active">
+                                <a href="stock.php"><i class="ti-dashboard"></i><span>Input Data</span></a>
+                            </li>
+							<li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout"></i><span>Transaksi Data
+                                    </span></a>
+                                <ul class="collapse">
+                                    <li><a href="masuk.php">Daftar Input</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="logout.php"><span>Logout</span></a>
+                                
+                            </li>
+                            
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- sidebar menu area end -->
+        <!-- main content area start -->
+        <div class="main-content">
+            <!-- header area start -->
+            <div class="header-area">
+                <div class="row align-items-center">
+                    <!-- nav and search button -->
+                    <div class="col-md-6 col-sm-8 clearfix">
+                        <div class="nav-btn pull-left">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <div class="search-box pull-left">
+                            <form action="#">
+                                <h2>Hi, Admin!</h2>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- profile info & task notification -->
+                    <div class="col-md-6 col-sm-4 clearfix">
+                        <ul class="notification-area pull-right">
+                            <li><h3><div class="date">
+								<script type='text/javascript'>
+						<!--
+						var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+						var myDays = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+						var date = new Date();
+						var day = date.getDate();
+						var month = date.getMonth();
+						var thisDay = date.getDay(),
+							thisDay = myDays[thisDay];
+						var yy = date.getYear();
+						var year = (yy < 1000) ? yy + 1900 : yy;
+						document.write(thisDay + ', ' + day + ' ' + months[month] + ' ' + year);		
+						//-->
+						</script></b></div></h3>
+
+						</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- header area end -->
+			<?php 
+			
+				$periksa_bahan=mysqli_query($conn,"select * from sstock_brg where stock <1");
+				while($p=mysqli_fetch_array($periksa_bahan)){	
+					if($p['stock']<=1){	
+						?>	
+						<script>
+							$(document).ready(function(){
+								$('#pesan_sedia').css("color","white");
+								$('#pesan_sedia').append("<i class='ti-flag'></i>");
+							});
+						</script>
+						<?php
+						echo "<div class='alert alert-danger alert-dismissible fade show'><button type='button' class='close' data-dismiss='alert'>&times;</button>Stok  <strong><u>".$p['nama']. "</u> <u>".($p['merk'])."</u> &nbsp <u>".$p['ukuran']."</u></strong> yang tersisa sudah habis</div>";		
+					}
+				}
+				?>
+			
+            <!-- page title area start -->
+            <div class="page-title-area">
+                <div class="row align-items-center">
+                    <div class="col-sm-6">
+                        <div class="breadcrumbs-area clearfix">
+                            <h4 class="page-title pull-left">Dashboard</h4>
+                            <ul class="breadcrumbs pull-left">
+                                <li><a href="index.php">Home</a></li>
+                                <li><span>Input Data</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 clearfix">
+                        
+                    </div>
+                </div>
+            </div>
+            <!-- page title area end -->
+            <div class="main-content-inner">
+               
+                <!-- market value area start -->
+                <div class="row mt-5 mb-5">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-sm-flex justify-content-between align-items-center">
+									<h2>Input Data Penduduk</h2>
+									
+                                </div>
+                                <div class="market-status-table mt-4">
+                                    <iframe src="https://forms.zohopublic.com/susrihaningsih28/form/DesaCantik2022/formperma/-mjcUzk7jFjBmAFd68WvWfmbn66jhoqDfs_bRm2Dflw" width="1000" height="500" frameborder="0" marginheight="0" marginwidth="0" >Memuat…</iframe>
+
+                                        </tbody>
+                                        </table>
+                                    </div>
+                                   
+                                                
+										</tbody>
+										</table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              
+                
+                <!-- row area start-->
+            </div>
+        </div>
+        <!-- main content area end -->
+        <!-- footer area start-->
+         
+        <footer class="main-footer">
+    <div class="pull-right">
+        <img src="../BPS.png" alt="BPS" width="40px">
+    </div>
+    <strong>Copyright &copy; 2022 Badan Pusat Statistik Kota Semarang</a></strong>
+</footer>
+
+
+        
+        <!-- footer area end-->
+    </div>
+   
+	<script>
+		$(document).ready(function() {
+		$('input').on('keydown', function(event) {
+			if (this.selectionStart == 0 && event.keyCode >= 65 && event.keyCode <= 90 && !(event.shiftKey) && !(event.ctrlKey) && !(event.metaKey) && !(event.altKey)) {
+			   var $t = $(this);
+			   event.preventDefault();
+			   var char = String.fromCharCode(event.keyCode);
+			   $t.val(char + $t.val().slice(this.selectionEnd));
+			   this.setSelectionRange(1,1);
+			}
+		});
+	});
+	
+	$(document).ready(function() {
+    $('#dataTable3').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'print'
+        ]
+    } );
+	} );
+	</script>
+	
+	<!-- jquery latest version -->
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <!-- bootstrap 4 js -->
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/metisMenu.min.js"></script>
+    <script src="assets/js/jquery.slimscroll.min.js"></script>
+    <script src="assets/js/jquery.slicknav.min.js"></script>
+		<!-- Start datatable js -->
+	 <script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js"></script>
+	<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
+    <!-- start chart js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+    <!-- start highcharts js -->
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <!-- start zingchart js -->
+    <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
+    <script>
+    zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
+    ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "ee6b7db5b51705a13dc2339db3edaf6d"];
+    </script>
+    <!-- all line chart activation -->
+    <script src="assets/js/line-chart.js"></script>
+    <!-- all pie chart -->
+    <script src="assets/js/pie-chart.js"></script>
+    <!-- others plugins -->
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/scripts.js"></script>
+	
+	
+</body>
+
+</html>
